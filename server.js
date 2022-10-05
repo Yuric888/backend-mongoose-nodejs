feee7055c9e7e0a73b1ad0b9b98be5e73c317996
Trung Nguyễn <yuricdev@gmail.com>
2022-10-05 15:50:15 +0700

    update: ket noi fe: Yuric

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false, limit: '30mb'}));
 // app.use()
 app.use(cors());
-
+app.use(cors({origin: 'http://localhost:3000'}));
 
 //init webrouter
 
