1073ef6f05a1f5a81f80f3679ddd85ed11be80bb
Trung Nguyễn <yuricdev@gmail.com>
2022-10-09 15:39:09 +0700

    fix-url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false, limit: '30mb'}));
 // app.use()
 app.use(cors());
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: process.env.URL_REACT}));
 
 //init webrouter
 
